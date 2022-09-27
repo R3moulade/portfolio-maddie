0a4b4446d78342e4dd8b7374de80797c6d448069
R3moulade <maddie.dyrby.hansen@outlook.dk>
2022-09-27 03:38:56 +0200

    stuff again

--- javascript.js
+++ javascript.js
@@ -1,9 +1,22 @@
 let popup = document.getElementById("popup");
+
 function openPopup() {
     popup.classList.add("open-popup");
 }
-
+let writingpopup = document.getElementById("writing-popup");
+function openWritingPopup() {
+    writingpopup.classList.add("open-popup");
+}
+let otherpopup = document.getElementById("other-popup");
+function openOtherPopup() {
+    otherpopup.classList.add("open-popup");
+}
 function closePopup() {
     popup.classList.remove("open-popup")
 }
-
+function closeWritingPopup() {
+    writingpopup.classList.remove("open-popup")
+}
+function closeOtherPopup() {
+    otherpopup.classList.remove("open-popup")
+}
